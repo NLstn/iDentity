726521c54292ec19549513df5cf16b3c247a0d7c
Niklas Lahnstein <niklas.lahnstein@gmail.com>
2020-10-17 09:53:59 +0000

    add main router

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const port = 3000
 
 const helmet = require('helmet')
 
+const mainRouter = require('./src/middlewares/mainrouter')
+
 app.use(helmet())
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+app.use('/api/v1', mainRouter);
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
