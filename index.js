cdaca8d304da48ab619be643a307fd1235cb5039
Niklas Lahnstein <niklas.lahnstein@gmail.com>
2020-10-17 08:52:21 +0000

    Basic express Setup

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const express = require('express')
+const app = express()
+const port = 3000
+
+const helmet = require('helmet')
+
+app.use(helmet())
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
+})
